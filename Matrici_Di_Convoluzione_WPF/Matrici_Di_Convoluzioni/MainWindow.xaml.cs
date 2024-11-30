10c90f319c15b99f20f0da4922247da887de4bb4
Leonardo Lessi <LEONARDO.LESSI@local.ittcesena.it>
2024-11-30 10:16:23 +0100

    a

---
+++ Matrici_Di_Convoluzione_WPF/Matrici_Di_Convoluzioni/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using SixLabors.ImageSharp.Processing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Matrici_Core;
+
+namespace Matrici_Di_Convoluzioni
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Matrice ma;
+        Immagine img;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            int[,] contrasto = new int[,];
+            contrasto[0, 1] = -1;
+            contrasto[1, 0] = -1;
+            contrasto[1, 1] = 5;
+            contrasto[1, 2] = -1;
+            contrasto[2, 1] = -1;
+
+            Matrice m = new Matrice(contrasto);
+
+        }
+
+        private void btnPercorsoFile_Click(object sender, RoutedEventArgs e)
+        {
+            img = new Immagine(txtPercorsoFile.Text);
+        }
+
+        private void lstMatrici_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ma = new Matrice(lstMatrici.SelectedItem as int[,], img);
+        }
+
+        private void btnApplicaMatrice_Click(object sender, RoutedEventArgs e)
+        {
+            ma.StartMatrice();
+            //metti l'immagine
+        }
+    }
+}
